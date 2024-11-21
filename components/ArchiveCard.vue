6cae8a7a8a02d8a0fc52b3daa9c3b4f7823a1bbb
wind-ik <soeag.st@gmail.com>
2024-11-21 23:28:37 +0900

    wip: past work design changed

--- components/ArchiveCard.vue
+++ components/ArchiveCard.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 defineProps<{
+  id: number;
   title: string;
   description: string;
   image: string;
@@ -10,24 +11,28 @@ defineProps<{
 
 <template>
   <div
-    class="flex flex-col gap-4 justify-evenly items-center text-gray-300 relative"
+    class="flex flex-col gap-4 justify-evenly text-gray-300 relative max-w-75"
+    :class="{ 'items-start': id % 2 === 1, 'items-end': id % 2 === 0 }"
   >
     <!-- <div
       class="absolute b-t-1 b-l-1 b-solid b-b-0 b-r-0 b-blue-400 top-0 left-0 w-4 h-4"
     /> -->
-    <div class="w-full sm:w-60% text-center">
-      <div class="flex gap-2 items-center justify-center">
+    <div class="w-full mt-2">
+      <div
+        class="flex gap-2"
+        :class="{ 'justify-start': id % 2 === 1, 'justify-end': id % 2 === 0 }"
+      >
         <NuxtLink
           class="decoration-none text-gray-200 hover:text-blue-400 font-semibold"
           :to="link"
         >
           <p class="text-sm text-wrap">{{ title }}</p>
         </NuxtLink>
-        <NuxtLink :to="github" target="_blank">
+        <!-- <NuxtLink :to="github" target="_blank">
           <div
             class="i-simple-icons-github text-white text-xl hover:text-blue-300"
           />
-        </NuxtLink>
+        </NuxtLink> -->
         <!-- <div class="i-material-symbols-open-in-new hover:cursor-pointer"></div> -->
       </div>
       <!-- <p class="text-gray-400 text-sm lg:text-base">
--- pages/works/index.vue
+++ pages/works/index.vue
@@ -1,45 +1,77 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Timeline from "primevue/timeline";
+
+const items = ref([
+  {
+    id: 1,
+    title: "Old Portfolio Website",
+    description: "work.description",
+    image: "/images/oldworks/part-old-port.png",
+    link: "https://portfolio0223.netlify.app/",
+    github: "https://github.com/soe-ag/portfolio_2023feb",
+  },
+  {
+    id: 2,
+    title: "Botha Competition Gallery Website",
+    description: "work.description",
+    image: "/images/oldworks/part-botha-gallery.png",
+    link: "https://lp.bo-tha.com/official_character_gallery/202212/",
+    github: "https://github.com/soe-ag/botha_compe_artlist",
+  },
+  {
+    id: 3,
+    title: "Botha Official Website",
+    description: "work.description",
+    image: "/images/oldworks/part-botha.png",
+    link: "https://soe-ag.github.io/botha_about_clone/",
+    github: "https://github.com/soe-ag/botha_about_clone",
+  },
+  {
+    id: 4,
+    title: "Botha Competition Website",
+    description: "work.description",
+    image: "/images/oldworks/part-botha-competition.png",
+    link: "https://soe-ag.github.io/botha_compete_clone/",
+    github: "https://github.com/soe-ag/botha_compete_clone",
+  },
+  {
+    id: 5,
+    title: "Egodiva Marketing Website",
+    description: "work.description",
+    image: "/images/oldworks/part-egodia-marketing.png",
+    link: "https://soe-ag.github.io/egodiva_marketing_clone/",
+    github: "https://github.com/soe-ag/egodiva_marketing_clone",
+  },
+]);
+</script>
 
 <template>
-  <div class="p-4 md:p-8 max-w-250 m-auto">
+  <div class="p-4 md:p-6 max-w-180 m-auto">
     <h3 class="text-center mb-8">Past Works</h3>
-    <section class="grid grid-cols-1 sm:grid-cols-2 gap-6">
-      <ArchiveCard
-        title="Old Portfolio Website"
-        description="work.description"
-        image="/images/oldworks/part-old-port.png"
-        link="https://portfolio0223.netlify.app/"
-        github="https://github.com/soe-ag/portfolio_2023feb"
-      />
+    <!-- <section class="grid grid-cols-1 sm:grid-cols-2 gap-6"></section> -->
 
-      <ArchiveCard
-        title="Botha Competition Gallery Website"
-        description="work.description"
-        image="/images/oldworks/part-botha-gallery.png"
-        link="https://lp.bo-tha.com/official_character_gallery/202212/"
-        github="https://github.com/soe-ag/botha_compe_artlist"
-      />
-      <ArchiveCard
-        title="Botha Official Website"
-        description="work.description"
-        image="/images/oldworks/part-botha.png"
-        link="https://soe-ag.github.io/botha_about_clone/"
-        github="https://github.com/soe-ag/botha_about_clone"
-      />
-      <ArchiveCard
-        title="Botha Competition Website"
-        description="work.description"
-        image="/images/oldworks/part-botha-competition.png"
-        link="https://soe-ag.github.io/botha_compete_clone/"
-        github="https://github.com/soe-ag/botha_compete_clone"
-      />
-      <ArchiveCard
-        title="Egodiva Marketing Website"
-        description="work.description"
-        image="/images/oldworks/part-egodia-marketing.png"
-        link="https://soe-ag.github.io/egodiva_marketing_clone/"
-        github="https://github.com/soe-ag/egodiva_marketing_clone"
-      />
-    </section>
+    <Timeline :value="items" align="alternate" class="customized-timeline">
+      <template #marker="slotProps">
+        <span
+          class="flex w-8 h-8 items-center justify-center text-white rounded-full z-10 shadow-sm"
+        >
+          <NuxtLink :to="slotProps.item.github" target="_blank">
+            <div
+              class="i-simple-icons-github text-white text-2xl hover:text-blue-300"
+            />
+          </NuxtLink>
+        </span>
+      </template>
+      <template #content="slotProps">
+        <ArchiveCard
+          :id="slotProps.item.id"
+          :title="slotProps.item.title"
+          :description="slotProps.item.description"
+          :image="slotProps.item.image"
+          :link="slotProps.item.link"
+          :github="slotProps.item.github"
+        />
+      </template>
+    </Timeline>
   </div>
 </template>
